6b870124988682aac56b28328cd19b516bec6ce7
pmqueiroz <pedroqueiroz593@gmail.com>
2020-10-22 20:33:17 -0300

    start home page

--- App.tsx
+++ App.tsx
@@ -1,11 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import { useFonts } from 'expo-font';
+import { Roboto_500Medium, Roboto_400Regular } from '@expo-google-fonts/roboto';
+
+import logoImg from './src/images/Logo.png';
 
 export default function App() {
+   const [fontsLoaded] = useFonts({
+      Roboto_500Medium, 
+      Roboto_400Regular
+   });
+
+   if(!fontsLoaded) {
+      return null;
+   }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Image source={logoImg} style={styles.logo}></Image>
+      <Text style={styles.title}>Select the game you wanna play</Text>
+      <View style={styles.buttonContainer}>
+         <RectButton onPress={() => {console.log('certo')} } style={styles.button}>
+            <Text>Tic Tac Toe</Text>
+         </RectButton>
+         <RectButton onPress={() => {console.log('certo')} } style={styles.button}>
+            <Text>Hangman</Text>
+         </RectButton>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +37,46 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F7F9F8',
     alignItems: 'center',
     justifyContent: 'center',
+    width: '100%',
+    height:' 100%'
   },
+
+  title: {
+     fontWeight: '500',
+     color: '#3E5A82',
+     fontFamily: 'Roboto_500Medium',
+     fontSize: 20,
+  },
+
+  logo: {
+   width: '90%',
+   minHeight: 200,
+   marginBottom: 60,
+  },
+
+  button: {
+     backgroundColor: '#3879F9',
+     width: '150px',
+     height: '50px',
+     color: '#FFF',
+     paddingVertical: 5,
+     paddingHorizontal: 10,
+     borderRadius: 70,
+     alignItems: 'center',
+     justifyContent: 'center',
+     marginTop: '10px',
+     fontFamily: 'Roboto_400Regular',
+     fontSize: 15,
+     shadowOffset:{  width: 0,  height: 5,  },
+     shadowColor: 'black',
+     shadowOpacity: 0.3,
+     shadowRadius: 5,
+  },
+
+  buttonContainer: {
+     marginTop: 40,
+  }
 });
