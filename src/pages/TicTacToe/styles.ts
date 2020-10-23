edf56aa1eed59e19d2f7c9edd31fe0b8d7a02794
kermit the frog <55104815+kaioschmitt@users.noreply.github.com>
2020-10-23 19:04:34 -0300

    fix(tic tac toe): spacing between players names

--- src/pages/TicTacToe/styles.ts
+++ src/pages/TicTacToe/styles.ts
@@ -8,6 +8,7 @@ const styles = StyleSheet.create({
       width: "100%",
       height: "100%",
    },
+   
    screen: {
       alignItems: "center",
       justifyContent: "space-around",
@@ -69,12 +70,14 @@ const styles = StyleSheet.create({
       color: "#3E5A82",
       fontFamily: "Roboto_500Medium",
       fontSize: 20,
+      flex: 1,
+      textAlign: 'center',
    },
 
-   
    activeX: {
       color: '#3879F9',
    },
+
    activeO: {
       color: "#F77634",
    },
