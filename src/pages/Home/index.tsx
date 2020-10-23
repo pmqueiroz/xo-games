b5370b0378f313aa0f1c6509ee1aecdb13dd5c6e
pmqueiroz <pedroqueiroz593@gmail.com>
2020-10-23 00:31:33 -0300

    fix: buttons not working on home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,24 +1,32 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, TouchableOpacity } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
+import { useNavigation } from '@react-navigation/native'
 
 import styles from './styles'
 import logoImg from '../../images/Logo.png';
 
 
 export default function Home(){
+   const navigation = useNavigation();
+
+   function handleNavigateToTicTacToe() {
+      console.log('clicked')
+      navigation.navigate('TicTacToe')      
+   }
+
    return (
       <View style={styles.container}>
       <Image source={logoImg} style={styles.logo}></Image>
       <Text style={styles.title}>Select the game you wanna play</Text>
       <View style={styles.buttonContainer}>
-         <RectButton onPress={() => {console.log('certo')} } style={styles.button}>
+         <TouchableOpacity onPress={handleNavigateToTicTacToe} style={styles.button}>
             <Text>Tic Tac Toe</Text>
-         </RectButton>
-         <RectButton onPress={() => {console.log('certo')} } style={styles.button}>
+         </TouchableOpacity>
+         <TouchableOpacity onPress={() => {console.log('certo')} } style={styles.button}>
             <Text>Hangman</Text>
-         </RectButton>
+         </TouchableOpacity>
       </View>
       <StatusBar style="auto" />
     </View>
--- src/routes.tsx
+++ src/routes.tsx
@@ -5,12 +5,14 @@ import { createStackNavigator } from '@react-navigation/stack';
 const {  Navigator, Screen } = createStackNavigator();
 
 import Home from './pages/Home';
+import TicTacToe from './pages/TicTacToe';
 
 export default function Routes() {
    return (
       <NavigationContainer>
          <Navigator screenOptions={{ headerShown: false }}>
             <Screen name="Home" component={Home}/>
+            <Screen name="TicTacToe" component={TicTacToe}/>
          </Navigator>
       </NavigationContainer>
    )
