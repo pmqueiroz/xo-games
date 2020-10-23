b4fe8acb85253df062c19d210762ba37ca07ea2e
Arthur Passos <arthur.opassos@gmail.com>
2020-10-23 11:53:21 -0300

    feat: scoreboard points

--- src/pages/TicTacToe/index.tsx
+++ src/pages/TicTacToe/index.tsx
@@ -1,55 +1,70 @@
-import React, { useState } from 'react';
-import { View, Image, TouchableOpacity } from 'react-native';
+import React, { useState } from "react";
+import { View, Image, TouchableOpacity, Text } from "react-native";
 
-import styles from './styles'
-import XImg from '../../images/X.png';
-import OImg from '../../images/O.png';
+import styles from "./styles";
+import XImg from "../../images/X.png";
+import OImg from "../../images/O.png";
+import CogImg from "../../images/Cog.png";
 
 export default function TicTacToe() {
    const initialBoardState = [
-      [0, 0, 0], 
-      [0, 0, 0], 
-      [0, 0, 0]
-   ]
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0],
+   ];
 
    const [currentPlayer, setCurrentPlayer] = useState(1);
    const [board, setBoard] = useState(initialBoardState);
 
+   const [scoreboard, setScoreboard] = useState([0, 0]);
+
+   const playerXName = "Player 1";
+   const playerOName = "Player 2";
 
    function renderIcon(row: number, column: number) {
       const value = board[row][column];
 
-      switch(value) {
-         case 1: return <Image source={XImg} style={styles.tables}/>;
-         case -1: return <Image source={OImg} style={styles.tables}/>;
-         default: return <View />;
+      switch (value) {
+         case 1:
+            return <Image source={XImg} style={styles.tables} />;
+         case -1:
+            return <Image source={OImg} style={styles.tables} />;
+         default:
+            return <View />;
       }
    }
 
    function onTilePress(row: number, column: number) {
-      const nextBoard = board.slice()
-      
-      if (nextBoard[row][column] !== 0){ return; }
+      const nextBoard = board.slice();
 
-      nextBoard[row][column] = currentPlayer;
-      setBoard(nextBoard)
-      const player = (currentPlayer == 1) ? -1 : 1;
-      setCurrentPlayer(player);
-
-      const resetGame = () =>{
-         setBoard(initialBoardState)
-         setCurrentPlayer(1);
+      if (nextBoard[row][column] !== 0) {
+         return;
       }
 
+      nextBoard[row][column] = currentPlayer;
+      setBoard(nextBoard);
+      const player = currentPlayer == 1 ? -1 : 1;
+      setCurrentPlayer(player);
+
+      const resetGame = () => {
+         setBoard(initialBoardState);
+         setCurrentPlayer(1);
+      };
+
       const winner = calculateWinner();
+      const currentScore = scoreboard.slice();
       if (winner === 1) {
-         console.log('Player X wins this game');
+         console.log("Player X wins this game");
+         currentScore[0]++;
+         setScoreboard(currentScore);
          resetGame();
       } else if (winner === -1) {
-         console.log('Player O wins this game');
+         console.log("Player O wins this game");
+         currentScore[1]++;
+         setScoreboard(currentScore);
          resetGame();
       } else if (winner === 0) {
-         console.log('Tied game');
+         console.log("Tied game");
          resetGame();
       }
    }
@@ -64,7 +79,7 @@ export default function TicTacToe() {
          if (check == 3) {
             return 1;
          } else if (check == -3) {
-            return -1
+            return -1;
          }
       }
 
@@ -74,7 +89,7 @@ export default function TicTacToe() {
          if (check == 3) {
             return 1;
          } else if (check == -3) {
-            return -1
+            return -1;
          }
       }
 
@@ -82,19 +97,19 @@ export default function TicTacToe() {
       if (check == 3) {
          return 1;
       } else if (check == -3) {
-         return -1
+         return -1;
       }
 
       check = currentGame[2][0] + currentGame[1][1] + currentGame[0][2];
       if (check == 3) {
          return 1;
       } else if (check == -3) {
-         return -1
+         return -1;
       }
 
       for (let i = 0; i < 3; i++) {
          for (let j = 0; j < 3; j++) {
-            if( currentGame[i][j] === 0) {
+            if (currentGame[i][j] === 0) {
                return 3;
             }
          }
@@ -102,62 +117,120 @@ export default function TicTacToe() {
 
       return 0;
    }
-   
+
    return (
       <View style={styles.container}>
+         <View style={styles.scoreboard}>
+            <Text style={styles.scoreText}>{playerXName}</Text>
+            <View style={styles.score}>
+               <Text style={styles.scoreText}>
+                  {scoreboard[0]} - {scoreboard[1]}
+               </Text>
+            </View>
+            <Text style={styles.scoreText}>{playerOName}</Text>
+         </View>
+
          <View style={styles.canvas}>
-            <View style={{flexDirection: 'row'}}>
-               <TouchableOpacity 
-                  onPress={ () => { onTilePress(0, 0) } } 
-                  style={[styles.tile, { borderLeftWidth: 0, borderTopWidth: 0 }]} 
+            <View style={{ flexDirection: "row" }}>
+               <TouchableOpacity
+                  onPress={() => {
+                     onTilePress(0, 0);
+                  }}
+                  style={[
+                     styles.tile,
+                     { borderLeftWidth: 0, borderTopWidth: 0 },
+                  ]}
                >
                   {renderIcon(0, 0)}
                </TouchableOpacity>
-               <TouchableOpacity 
-                  onPress={ () => { onTilePress(0, 1) } } 
-                  style={[styles.tile, { borderTopWidth: 0 }]} 
+               <TouchableOpacity
+                  onPress={() => {
+                     onTilePress(0, 1);
+                  }}
+                  style={[styles.tile, { borderTopWidth: 0 }]}
                >
                   {renderIcon(0, 1)}
                </TouchableOpacity>
-               <TouchableOpacity 
-                  onPress={ () => { onTilePress(0, 2) } } 
-                  style={[styles.tile, { borderRightWidth: 0, borderTopWidth: 0 }]} 
+               <TouchableOpacity
+                  onPress={() => {
+                     onTilePress(0, 2);
+                  }}
+                  style={[
+                     styles.tile,
+                     { borderRightWidth: 0, borderTopWidth: 0 },
+                  ]}
                >
                   {renderIcon(0, 2)}
                </TouchableOpacity>
             </View>
-            <View style={{flexDirection: 'row'}}>
-               <TouchableOpacity 
-               onPress={ () => { onTilePress(1, 0) } } 
-               style={[styles.tile, { borderLeftWidth: 0 }]} 
+            <View style={{ flexDirection: "row" }}>
+               <TouchableOpacity
+                  onPress={() => {
+                     onTilePress(1, 0);
+                  }}
+                  style={[styles.tile, { borderLeftWidth: 0 }]}
                >
                   {renderIcon(1, 0)}
                </TouchableOpacity>
-               <TouchableOpacity 
-                  onPress={ () => { onTilePress(1, 1) } } 
-                  style={styles.tile} 
+               <TouchableOpacity
+                  onPress={() => {
+                     onTilePress(1, 1);
+                  }}
+                  style={styles.tile}
                >
                   {renderIcon(1, 1)}
                </TouchableOpacity>
-               <TouchableOpacity 
-                  onPress={ () => { onTilePress(1, 2) } } 
-                  style={[styles.tile, { borderRightWidth: 0 }]} 
+               <TouchableOpacity
+                  onPress={() => {
+                     onTilePress(1, 2);
+                  }}
+                  style={[styles.tile, { borderRightWidth: 0 }]}
                >
                   {renderIcon(1, 2)}
                </TouchableOpacity>
             </View>
-            <View style={{flexDirection: 'row'}}>
-               <TouchableOpacity onPress={ () => { onTilePress(2, 0) } } style={[styles.tile, { borderLeftWidth: 0, borderBottomWidth: 0 }]} >
+            <View style={{ flexDirection: "row" }}>
+               <TouchableOpacity
+                  onPress={() => {
+                     onTilePress(2, 0);
+                  }}
+                  style={[
+                     styles.tile,
+                     { borderLeftWidth: 0, borderBottomWidth: 0 },
+                  ]}
+               >
                   {renderIcon(2, 0)}
                </TouchableOpacity>
-               <TouchableOpacity onPress={ () => { onTilePress(2, 1) } } style={[styles.tile, { borderBottomWidth: 0 }]} >
+               <TouchableOpacity
+                  onPress={() => {
+                     onTilePress(2, 1);
+                  }}
+                  style={[styles.tile, { borderBottomWidth: 0 }]}
+               >
                   {renderIcon(2, 1)}
                </TouchableOpacity>
-               <TouchableOpacity onPress={ () => { onTilePress(2, 2) } } style={[styles.tile, { borderRightWidth: 0, borderBottomWidth: 0 }]} >
+               <TouchableOpacity
+                  onPress={() => {
+                     onTilePress(2, 2);
+                  }}
+                  style={[
+                     styles.tile,
+                     { borderRightWidth: 0, borderBottomWidth: 0 },
+                  ]}
+               >
                   {renderIcon(2, 2)}
                </TouchableOpacity>
             </View>
          </View>
+
+         <TouchableOpacity
+            style={styles.config}
+            onPress={() => {
+               setScoreboard([0, 0]);
+            }}
+         >
+            <Image style={styles.buttonIcon} source={CogImg}></Image>
+         </TouchableOpacity>
       </View>
-   )
-};
+   );
+}
--- src/pages/TicTacToe/styles.ts
+++ src/pages/TicTacToe/styles.ts
@@ -1,22 +1,21 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
    container: {
-     flex: 1,
-     backgroundColor: '#F7F9F8',
-     alignItems: 'center',
-     justifyContent: 'center',
-     width: '100%',
-     height:' 100%'
+      backgroundColor: "#F7F9F8",
+      alignItems: "center",
+      justifyContent: "space-around",
+      width: "100%",
+      height: "100%",
    },
 
    tile: {
       borderWidth: 1,
-      borderColor: '#ADA6A6',
+      borderColor: "#ADA6A6",
       width: 100,
       height: 100,
-      alignItems: 'center',
-      justifyContent: 'center',
+      alignItems: "center",
+      justifyContent: "center",
    },
 
    tables: {
@@ -25,16 +24,63 @@ const styles = StyleSheet.create({
    },
 
    canvas: {
-      backgroundColor: '#FFFFFF',
-      alignItems: 'center',
-      justifyContent: 'center',
-      shadowOffset:{  width: 0,  height: 0,  },
-      shadowColor: 'black',
+      backgroundColor: "#FFFFFF",
+      alignItems: "center",
+      justifyContent: "center",
+      shadowOffset: { width: 0, height: 0 },
+      shadowColor: "black",
       shadowOpacity: 0.3,
       shadowRadius: 30,
       padding: 20,
       borderRadius: 20,
-   }
- })
+   },
 
- export default styles;
+   scoreboard: {
+      width: "100%",
+      justifyContent: "center",
+      alignItems: "center",
+      flexDirection: "row",
+   },
+
+   score: {
+      backgroundColor: "#FFFFFF",
+      width: 70,
+      height: 30,
+      borderRadius: 50,
+      justifyContent: "center",
+      alignItems: "center",
+      marginHorizontal: 20,
+      shadowOffset: { width: 0, height: 0 },
+      shadowColor: "black",
+      shadowOpacity: 0.3,
+      shadowRadius: 10,
+   },
+
+   scoreText: {
+      fontWeight: "500",
+      color: "#3E5A82",
+      fontFamily: "Roboto_500Medium",
+      fontSize: 20,
+   },
+
+   config: {
+      backgroundColor: "#FFFFFF",
+      width: 50,
+      height: 50,
+      borderRadius: 50,
+      justifyContent: "center",
+      alignItems: "center",
+      shadowOffset: { width: 0, height: 0 },
+      shadowColor: "black",
+      shadowOpacity: 0.3,
+      shadowRadius: 10,
+   },
+
+   buttonIcon: {
+      width: 35,
+      height: 35,
+      resizeMode: "contain",
+   },
+});
+
+export default styles;
