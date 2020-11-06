a6c8ac70ebd13edfe520bccdfc4b04f9eb449447
pmqueiroz <pedroqueiroz593@gmail.com>
2020-11-06 12:34:36 -0300

    chore(Hangman): create and style choose nickname page

--- src/pages/Hangman/ChooseNickname/styles.ts
+++ src/pages/Hangman/ChooseNickname/styles.ts
@@ -1,9 +1,29 @@
 import styled from 'styled-components/native';
 
+export const Container = styled.View`
+   flex: 1;
+   justify-content: space-between;
+   align-items: center;
+   padding: 20% 0;
+`;
+
 export const PlayerNameInput = styled.TextInput`
    background-color: #FFF;
    width: 95%;
    height: 75%;
    border-radius: 50px;
    padding-left: 10px;
+`;
+
+export const Title = styled.Text`
+   font-weight: 500;
+   color: #3E5A82;
+   font-family: 'Roboto_500Medium';
+   font-size: 20px;
+   text-align: center;
+`;
+
+export const ControlImg = styled.Image`
+   width: 156px;
+   height: 69px;
 `;
