e9d131834cf538df8a370d7b70c9e946fefd0506
pmqueiroz <pedroqueiroz593@gmail.com>
2020-11-06 10:09:16 -0300

    chore(Hangman): create and style home page

--- src/pages/Hangman/Menu/index.tsx
+++ src/pages/Hangman/Menu/index.tsx
@@ -1,12 +1,56 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
-// import { Container } from './styles';
+import LogoImg from '../../../images/hangman-logo.png';
+import {
+  Container, Screen, ButtonsContainer, Logo, Title,
+} from './styles';
+import Button from '../../../Components/Button';
 
-const HMMenu: React.FC = () => (
-  <View>
-    <Text>Welcome to hangman, play or edit collections to play</Text>
-  </View>
-);
+const HMMenu: React.FC = () => {
+  const navigation = useNavigation();
+
+  function handleNavigateToHangmanGame() {
+    // cleiton
+  }
+  function handleIDunnoWTFThisShitGonnaDo() {
+    // cleiton
+  }
+  function handleNavigateToHomePage() {
+    navigation.navigate('Home');
+  }
+
+  return (
+    <Container>
+      <Screen>
+        <Logo source={LogoImg} />
+        <Title>Welcome to hangman, play or edit collections to play</Title>
+        <ButtonsContainer>
+          <Button
+            onPress={handleNavigateToHangmanGame}
+            backgroundColor="#3879F9"
+            textColor="#FFF"
+          >
+            Play
+          </Button>
+          <Button
+            onPress={handleIDunnoWTFThisShitGonnaDo}
+            backgroundColor="#3879F9"
+            textColor="#FFF"
+          >
+            Collections
+          </Button>
+          <Button
+            onPress={handleNavigateToHomePage}
+            backgroundColor="#FFF"
+            textColor="#000"
+          >
+            Home
+          </Button>
+        </ButtonsContainer>
+      </Screen>
+    </Container>
+  );
+};
 
 export default HMMenu;
---
+++ src/pages/Hangman/Menu/styles.ts
@@ -0,0 +1,32 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+   flex: 1;
+   justify-content: center;
+   align-items: center;
+`;
+
+export const Screen = styled.View`
+   justify-content: space-between;
+   align-items: center;
+   width: 90%;
+   height: 80%;
+`;
+
+export const ButtonsContainer = styled.View`
+   justify-content: center;
+   align-items: center;
+`;
+
+export const Logo = styled.Image`
+   width: 167px;
+   height: 146px;
+`;
+
+export const Title = styled.Text`
+   font-weight: 500;
+   color: #3E5A82;
+   font-family: 'Roboto_500Medium';
+   font-size: 20px;
+   text-align: center;
+`;
